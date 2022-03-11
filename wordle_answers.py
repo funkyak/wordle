9250d72d96249888f814ffff1d09cf42193699e4
da123M <34272259+da123M@users.noreply.github.com>
2022-03-11 23:28:44 +0000

    Add files via upload

--- wordle_answers.py
+++ wordle_answers.py
@@ -4,6 +4,11 @@
 
 
 
+from asyncio import create_subprocess_shell
+import gettext
+
+
+
 with open("words1.txt","r") as words:
     contents = words.read()
     
@@ -19,22 +24,28 @@ def search():
             
             if Yesterdays_Wordle in contents:
                 print('Word found!')
+
+               # today_wordle = Yesterdays_Wordle 
+                def getNext(words):                
+                    next_el = None
+                    for index, elem in enumerate(contents):
+                        for index, elem in enumerate(contents):
+                            if index + 1 <len(contents) and index - 1 >= 0:
+                                contents = elem - 1
+                                if words == contents:
+                                    next_el = contents[index]
+                                    print(next_el)
+                    return next_el
+                print(gettext)
+            getNext()
+
             
 
-
-            
-            else:
-                print("Fail")
-
-            # loop didnot work 
-            
+                    
 
             ## possible loop function needed to search through this. 
             
             #Todays_wordle = Yesterdays_Wordle + len(words + 1)
-            #print("Todays_wordle")
-            ##print(f'line{count}: {line}')
-            ## not jumping to the new lines always line 1
             
 
        
