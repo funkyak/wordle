ef58d1bd55fc5f7ba1bcedd1d74f26d1e95096f0
da123M <34272259+da123M@users.noreply.github.com>
2022-03-12 21:21:18 +0000

    Add files via upload

--- wordle_answers.py
+++ wordle_answers.py
@@ -1,15 +1,16 @@
+
+import webbrowser
+webbrowser.open('https://www.nytimes.com/games/wordle/index.html')
+
 with open("words1.txt","r") as words:
     contents = words.read()
     
+# url https://www.nytimes.com/games/wordle/index.html
 
 def search():
-        lines = []
-        #file with word list 
-        with open("words1.txt","r") as words:
-            lines = words.readlines()
-            
+
             #input yesterdays wordle
-            Yesterdays_Wordle = input("Yesterdays Wordle: ")
+            Yesterdays_Wordle = input("Yesterdays Wordle : ")
             
             if Yesterdays_Wordle in contents:
                 index = contents
@@ -27,4 +28,9 @@ def search():
                 #Output 
                 print("Todays wordle is : "+ next_word + next_word1 + next_word2 + next_word3 + next_word4)
                 
+            else:
+                print("Not Found")
+
 search()
+
+
