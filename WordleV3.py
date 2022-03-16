7a7eb797875a44ae5b188562c781be3b92c061c1
da123M <34272259+da123M@users.noreply.github.com>
2022-03-16 09:26:24 +0000

    Add files via upload

--- WordleV3.py
+++ WordleV3.py
@@ -2,6 +2,7 @@ import webbrowser
 import time
 from pynput.keyboard import Key, Controller
 
+
 keyboard = Controller()
 
 #Word list file (Same Directory as this script)
@@ -41,24 +42,26 @@ def search():
   #Output 
         today = next_word + next_word1 + next_word2 + next_word3 + next_word4
         print("Todays wordle is : "+ today)
-        with open('yesterdays.txt', "w") as today_wordle:
-            today_wordle.write(today)
-        time.sleep(1)
+
+        def previous():
+            back1 = words[words.index(Yesterdays_Wordle)- 13]
+            back2 = words[words.index(Yesterdays_Wordle)- 12]
+            back3 = words[words.index(Yesterdays_Wordle)- 11]
+            back4 = words[words.index(Yesterdays_Wordle)- 10]
+            back5 = words[words.index(Yesterdays_Wordle)-9]
+            previous = back1 + back2+ back3 + back4 + back5
+            print("Previous wordle is :" + previous)
+        
 
     else:
         #Prints Error Message
         print("Not Found")
+    
 
-    def website():
-        print("Now opening website to enter word")
-        time.sleep(1.5)
-        #opening the TEST website
-        webbrowser.open('file:///C:/Users/Rory/Documents/wordle.html')
-        time.sleep(3)
-    website()
-
-  # Entering todays word on the website
+    # Entering todays word on the website
     def Entering():
+        time.sleep(2)
+        #Working DO NOT change!!!!!!!
         time.sleep(.12)
         keyboard.press(next_word)
         keyboard.release(next_word)
@@ -77,6 +80,57 @@ def search():
         time.sleep(.12)
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
+    
+
+    def website():
+
         
-    Entering()
+        print ('1 -- Test website' )
+        print ('2 -- Live Website' )
+        # Live Website Loads new wordle into the text file 
+        print ('3 -- Checking word' )
+        print ('4 -- New word' )
+        print ('5 -- Load yesterdays word')
+        print ('6 -- Exit' )
+        option = int(input('Enter your choice: ')) 
+
+        if option == 1:
+            print("Loading Test website")
+            webbrowser.open('file:///C:/Users/Rory/Documents/wordle.html')
+            
+            Entering()
+
+
+        elif option == 2:
+            print("Loading Live Website")
+            webbrowser.open('https://www.nytimes.com/games/wordle/index.html')
+            Entering()
+
+        elif option == 3:
+            print(yesterdays)
+
+        elif option == 4:
+            with open('yesterdays.txt', "w") as today_wordle:
+                today_wordle.write(today)
+                print(today)
+        
+        elif option == 5:
+            with open('yesterdays.txt', "w") as today_wordle:
+                today_wordle.write(previous)
+                print(previous)
+
+
+        elif option == 6:
+            #exit function
+            print('BYE :-)')
+            exit()
+
+        else:
+            #Error function
+            print("Please enter between 1 - 3")
+
+        time.sleep(3)
+        
+    website()
+
 search()
